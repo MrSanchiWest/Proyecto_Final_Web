f5805a0d91f87524bc654e4b00a8a6f5e23c6d77
sergi <sergi@LAPTOP-RA3CROUK>
2024-06-07 22:27:33 -0600

    javascript

---
+++ web/LibJavaScript.js
@@ -0,0 +1,10 @@
+
+
+function miFunc() {
+    // Ocultar el mensaje
+    document.getElementById("Mensaje").style.display = "none";
+    
+    // Mostrar el formulario y el inventario
+    document.getElementById("Formulario").style.display = "block";
+    document.getElementById("TablaInventario").style.display = "block";
+}
